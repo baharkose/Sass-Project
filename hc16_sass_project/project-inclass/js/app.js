b14277ca91566cb1e02494b31e0097e2dff2029b
baharkose <baharkse17@gmail.com>
2023-11-09 23:40:56 +0300

    adding media query to navbar

--- hc16_sass_project/project-inclass/js/app.js
+++ hc16_sass_project/project-inclass/js/app.js
@@ -1,1 +1,10 @@
 //? JS
+
+const hamburgerBtn = document.querySelector(".fa-bars")
+
+    hamburgerBtn.addEventListener("click", ()=>{
+    const headerLinks = document.querySelector(".header__links")
+    headerLinks.classList.toggle("show")
+    // headerlinksin classlistine show'u ekle.
+
+})
